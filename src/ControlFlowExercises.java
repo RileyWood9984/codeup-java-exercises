56e325c5d027c788ba4cbdb7eed9bebddbef980a
RileyWood9984 <rjw9984@gmail.com>
2023-03-27 14:57:10 -0500

    fixed

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,7 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
-        for(int i = 2; i<10000;i*=i){
+        for(long i = 2; i<1000000;i*=i){
               System.out.printf("%s ",i);
-              if(i==256){
-                  System.out.print(i*i);
-              }
           }
         }
 
