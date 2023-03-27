79c0f0ee587693a1f46c33495ecc8a93bd5cb3fd
RileyWood9984 <rjw9984@gmail.com>
2023-03-27 14:00:48 -0500

    do while part1

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,11 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
-        int i =5;
-        while( i <= 15){
-            System.out.printf("%s ", i);
-            ++i;
-        }
+       int i = 0;
+       do {
+           System.out.printf("%s ",i);
+           ++i;
+           ++i;
+        }while (i<=100);
 
     }
 }
