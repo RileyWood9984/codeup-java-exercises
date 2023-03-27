84ca3a2e509f81ca29e257c035d8b91c8ea3f6f5
RileyWood9984 <rjw9984@gmail.com>
2023-03-27 16:00:52 -0500

    exercises done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,23 +4,18 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What number would you like to up to?");
+        System.out.println("What is your grade out of one hundred?");
         int userNum= scanner.nextInt();
-        if (userNum > 1) {
-            for (int i = 1; i <= userNum; i++) {
-                int squared = i * i;
-                int cubed = i * i * i;
-                if (i == 1) {
-                    System.out.println("Number | Squared | Cubed");
-                    System.out.println("------ | ------- | -----");
-                    System.out.println("1      | 1       | 1    ");
-                } else {
-                    System.out.printf("%s      | %s       | %s    %n", i, squared, cubed);
-
-                }
-
-            }
-
+        if (userNum <= 59) {
+            System.out.println("That's an F.");
+            } else if (userNum<=66) {
+            System.out.println("A D is not that bad!");
+        } else if(userNum<=79){
+            System.out.println("A C is pretty good.");
+        } else if(userNum<=87){
+            System.out.println("A B is close to the best, nice!");
+        }else {
+            System.out.println("Well done on the A!");
         }
     }
 
