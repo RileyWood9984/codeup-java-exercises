edb8dcafe992fefcec86eb2ca725a009a77d5fb6
RileyWood9984 <rjw9984@gmail.com>
2023-03-27 15:19:54 -0500

    fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,8 +1,19 @@
 public class ControlFlowExercises {
-    public static void main(String[] args){
-        for(long i = 2; i<1000000;i*=i){
-              System.out.printf("%s ",i);
-          }
-        }
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i%3== 0 && 0==i%5) {
+                System.out.println("FizzBuzz");
+                continue;
+            } else if (i%3==0) {
+                System.out.println("Fizz");
+                continue;
 
+            }else if(i%5==0){
+                    System.out.println("Buzz");
+                    continue;
+            }
+            System.out.printf("%s%n", i);
+        }
     }
+
+}
