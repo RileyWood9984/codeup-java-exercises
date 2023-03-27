2ae496afc65808712a22ef67b967bd7ba3eed865
RileyWood9984 <rjw9984@gmail.com>
2023-03-27 15:51:56 -0500

    table

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,18 +1,26 @@
+
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i%3== 0 && 0==i%5) {
-                System.out.println("FizzBuzz");
-                continue;
-            } else if (i%3==0) {
-                System.out.println("Fizz");
-                continue;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What number would you like to up to?");
+        int userNum= scanner.nextInt();
+        if (userNum > 1) {
+            for (int i = 1; i <= userNum; i++) {
+                int squared = i * i;
+                int cubed = i * i * i;
+                if (i == 1) {
+                    System.out.println("Number | Squared | Cubed");
+                    System.out.println("------ | ------- | -----");
+                    System.out.println("1      | 1       | 1    ");
+                } else {
+                    System.out.printf("%s      | %s       | %s    %n", i, squared, cubed);
 
-            }else if(i%5==0){
-                    System.out.println("Buzz");
-                    continue;
+                }
+
             }
-            System.out.printf("%s%n", i);
+
         }
     }
 
