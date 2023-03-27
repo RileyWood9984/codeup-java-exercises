a833527ae5b6ec839e1b960ceaf92233954cc54e
RileyWood9984 <rjw9984@gmail.com>
2023-03-27 14:54:10 -0500

    for loop but for reason it breaks and only prints 0 after 256

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,8 +1,10 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
-        for(int i = 100; i>=-10;i-=5){
+        for(int i = 2; i<10000;i*=i){
               System.out.printf("%s ",i);
-
+              if(i==256){
+                  System.out.print(i*i);
+              }
           }
         }
 
